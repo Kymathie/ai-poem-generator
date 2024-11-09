a668f6c23f41bb3be26dc609fd28e1127ada72df
Kymathie <kymalexander93@gmail.com>
2024-11-09 17:25:53 +0000

    improved the UX

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ function showPoem(response){
 function generatePoem(event){
     event.preventDefault()
 
-let poemWaiting = document.querySelector("#poem")
-poemWaiting.innerHTML = "Generating poem. Please wait..."
+
 
 let poemInput = document.querySelector("#user-input")
     let apiKey = "28dfoad314b78930c81640808f41tf65"
@@ -20,6 +19,11 @@ let poemInput = document.querySelector("#user-input")
     let context = "please make a short poem with four lines and separate each line with a <br />"
     let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
     
+    let poemWaiting = document.querySelector("#poem")
+    poemWaiting.innerHTML = `<div class="blink"> Generating poem about ${poemInput.value}. Please wait...</div>`
+    poemWaiting.classList.remove("hidden")
+
+
     axios.get(apiUrl).then(showPoem)
    
 }
