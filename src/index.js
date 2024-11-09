0cacb9f9b4a245b3f9670c006fd1c3b6bb8ae31b
Kymathie <kymalexander93@gmail.com>
2024-11-09 15:58:20 +0000

    added api integration and some adjustments in css and HTML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generatePoem(event){
-    event.preventDefault()
-
-    new Typewriter('#poem', {
-        strings: "In the early morning sun",
+function showPoem(response){
+    new Typewriter("#poem", {
+        strings: response.data.answer,
         autoStart: true,
         cursor: null,
         delay: 40,
       });
+      console.log(response.data.answer)
+}
+
+function generatePoem(event){
+    event.preventDefault()
+
+let poemWaiting = document.querySelector("#poem")
+poemWaiting.innerHTML = "Generating poem. Please wait..."
+
+let poemInput = document.querySelector("#user-input")
+    let apiKey = "28dfoad314b78930c81640808f41tf65"
+    let prompt = `generate a short poem about ${poemInput.value}`
+    let context = "please make a short poem with four lines and separate each line with a <br />"
+    let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+    
+    axios.get(apiUrl).then(showPoem)
+   
 }
 
 
